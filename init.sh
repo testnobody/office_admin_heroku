7ac619d9ef1a117901bd8a2c113671bb84ad5081
testnobody <70645370+testnobody@users.noreply.github.com>
2021-05-01 21:20:32 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,18 @@ cat << EOF > /var/www/html/config.php
 return [
 	//全局账号相关配置
 	'accounts'=>[
-		$count
+		[
+			'name'=>'$name',
+			'client_id'=>'$client_id',
+			'tenant_id'=>'$tenant_id',
+			'client_secret'=>'$client_secret',
+			'sku_ids'=>[
+				'a1学生'=>'314c4481-f395-4525-be8b-2ec4bb1e9d91',
+				'a1教职工'=>'94763226-9b3c-4e75-a931-5c89701abe66',
+				'a1p学生'=>'e82ae690-a2d5-4d76-8d30-7c6e01e6022e',
+				'a1p教职工'=>'78e66a63-337a-4a9a-8959-41c6654dfb56'
+			],
+		],
 	],
 	
 	
